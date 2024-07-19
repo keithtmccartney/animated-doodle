197254c8fc1dd10572b076c888cd8a79931cabb7
Keith McCartney <keithtmccartney@hotmail.com>
2024-07-19 08:03:23 +0100

    Adding base interfaces and classes for the repositories

---
+++ animated-doodle/animated-doodle.Data/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animated_doodle.Data.Interfaces
+{
+    public interface ICourseRepository
+    {
+    }
+}
---
+++ animated-doodle/animated-doodle.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using animated_doodle.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animated_doodle.Data.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+    }
+}
